8f33ad7bc1d7fe58bf7c8a1d40fa62f6a5871907
Gary Fenstamaker <fenstamaker@gmail.com>
2015-02-16 21:23:36 -0500

    Everything

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,4 @@
 (function($) {
     hljs.initHighlightingOnLoad();
-    try {
-	// This is needed to prevent an uncaught
-	// type error when there is no footnote section
-	$('.post').sidenotes();
-    } catch (e) {};
+
 })($);
