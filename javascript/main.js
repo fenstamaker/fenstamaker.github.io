eb3651cbc16c6a8e4b94ce066934fcb31badd8dc
Gary Fenstamaker <fenstamaker@gmail.com>
2015-02-22 22:13:19 -0500

    Post.html

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,10 @@
 (function($) {
     hljs.initHighlightingOnLoad();
-
+    $.featherlight.defaults.afterOpen = function() {
+        $('body').addClass("noscroll");
+    };
+    $.featherlight.defaults.afterClose = function() {
+        $('body').removeClass("noscroll");
+    };
+    $('.lightbox').featherlightGallery();
 })($);
